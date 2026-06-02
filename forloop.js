5773fa568d3b05fe9883e2f0d963861d7f8cff2d
mathan patel <manthanpatel966@gmail.com>
2026-06-02 17:53:11 +0530

    for loop

---
+++ forloop.js
@@ -0,0 +1,62 @@
+ const products = [
+    {
+      name: "Iphone",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRke_DT4zYuoEPaB8wiUXCzSC9r-2mXq2rNDw&s",
+      description: "Latest Android smartphone with AMOLED display.",
+      price: "₹1,25,000"
+    },
+    {
+      name: "Laptop",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPl5ImxEFrWjFDb1nCYnKt-5q1gJVDFU93bA&s",
+      description: "16GB RAM laptop perfect for coding and work.",
+      price: "₹1,55,000"
+    },
+    {
+      name: "Headphones",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFu_cTufrdEN3GNdDwnvWQm4X1u5_srpy2VA&s",
+      description: "Wireless headphones with noise cancellation.",
+      price: "₹3,000"
+    },
+    {
+      name: "Smartwatch",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLnYgWP-F7sLaGnjC60XwCRaWceQy3EFpk-Q&s",
+      description: "Track fitness and notifications easily.",
+      price: "₹44,500"
+    },
+    {
+      name: "Camera",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZxKESZdOpt9ZdKmXedc5L3E-WTEj49HJqoQ&s",
+      description: "Professional DSLR for photography.",
+      price: "₹1,65,000"
+    },
+    {
+      name: "Gaming Console",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyKDJ2Fge00KoYxhEnyegYkVFRkMAZcfU4ng&s",
+      description: "Next-gen console for immersive gaming.",
+      price: "₹80,000"
+    },
+    {
+      name: "Wireless Mouse",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqdfWad55In7CR5v47plMLj5JqFRM7_vw_6Q&s",
+      description: "Ergonomic mouse with smooth tracking.",
+      price: "₹800"
+    },
+    {
+      name: "Keyboard",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR0IkgtRWWfoy_rFDvUkMn13CViBSGPxA68A&s",
+      description: "Mechanical keyboard with RGB lights.",
+      price: "₹3,500"
+    },
+    {
+      name: "Smart TV",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu45zsEcrW__hiRkHICvScQCZ51wJZnAhYSg&s",
+      description: "4K Ultra HD Smart TV with apps.",
+      price: "₹45,000"
+    },
+    {
+      name: "Power Bank",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAS8mkRju-Ws-7KPN9Y15PvJcOn0eVoeSbug&s",
+      description: "20000mAh fast charging power bank.",
+      price: "₹2,200"
+    }
+  ];
